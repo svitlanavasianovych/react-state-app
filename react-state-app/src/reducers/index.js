1c9572d822b49730ae718e8beb8ca2db3b9377b5
svasyanovych <svitlana.v92@gmail.com>
2020-04-21 22:31:15 +0300

    added action and reducer fun

---
+++ react-state-app/src/reducers/index.js
@@ -0,0 +1,27 @@
+const initialState = {
+    dog: 0,
+    cat: 0,
+    parrot: 0
+}
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case 'VOTE_DOG':
+            return Object.assign({}, state, {
+                dog: state.dog + 1
+            })
+
+        case 'VOTE_CAT':
+            return Object.assign({}, state, {
+                cat: state.cat + 1
+            })
+
+        case 'VOTE_PARROT':
+            return Object.assign({}, state, {
+                parrot: state.parrot + 1
+            })
+
+        default:
+            return state
+    }
+}
